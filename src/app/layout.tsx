fdeac6cf2c6ca4cd1ef09edad370cc294161f7fa
Italo Leal <itlcoorporativo@gmail.com>
2024-02-17 22:47:55 -0300

    Cria layout da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className={inter.variable} lang="pt">
-      <body >{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
